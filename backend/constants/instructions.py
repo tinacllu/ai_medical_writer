c7a49292c5b930168cfcb4069a128760d5bdb2bc
Tina Lu <tina.lu@vial.com>
2025-12-14 19:07:10 -0500

    fix: update instructions

--- backend/constants/instructions.py
+++ backend/constants/instructions.py
@@ -1,9 +1,10 @@
 BRIEF_INSTRUCTIONS = f"""
-Write a short paragraph giving a disease overview with maximum 7 sentences. List the references used to provide the information in the paragraph using APA format.
+Write a short paragraph with the following sections in proper markdown format. The paragraph should provide a disease overview with maximum 7 sentences under the header "Overview". 
+List the references used to provide the information in the paragraph using APA format under the header "References".
 """
 
 DETAILED_INSTRUCTIONS = f"""
-Include the following sections: 
+Include the following sections in markdown format with proper markdown headers: 
 1. Overview
 2. Symptoms
 3. Mechanism of Disease 
